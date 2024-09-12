847a6e6431d8617a9be33c574a176a0032e9f924
maru <r0mirov@yandex.ru>
2024-09-13 00:33:03 +0300

    init commit

---
+++ src/main/kotlin/com/example/openapi/OpenApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.openapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenApiApplication>(*args)
+}
