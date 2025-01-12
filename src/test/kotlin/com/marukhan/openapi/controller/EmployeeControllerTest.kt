ada805740164a37863e4ca4407e8295a0bfe3a10
maru <romirov@gmail.com>
2025-01-12 22:27:21 +0300

    fixed tests

--- src/test/kotlin/com/marukhan/openapi/controller/EmployeeControllerTest.kt
+++ src/test/kotlin/com/marukhan/openapi/controller/EmployeeControllerTest.kt
@@ -2,8 +2,8 @@ package com.marukhan.openapi.controller
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.marukhan.openapi.AbstractTestWithoutDb
-import com.marukhan.openapi.model.request.EmployeeRq
-import com.marukhan.openapi.model.request.EmployeeRs
+import com.marukhan.openapi.dao.EmployeesTest.employeeRq1
+import com.marukhan.openapi.dao.EmployeesTest.employeeRs1
 import com.marukhan.openapi.service.EmployeeService
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.coVerify
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import java.util.UUID
 
 class EmployeeControllerTest : AbstractTestWithoutDb() {
 	@MockkBean(relaxed = true)
@@ -20,36 +19,17 @@ class EmployeeControllerTest : AbstractTestWithoutDb() {
 
 	@Test
 	fun `add Employee test`() {
-		every { employeeService.save(any()) } returns employee
+		every { employeeService.save(any()) } returns employeeRs1
 		mockMvc.perform(
 			MockMvcRequestBuilders.post("/api/v1/employee")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(jacksonObjectMapper().writeValueAsString(createEmployeeRq))
+				.content(jacksonObjectMapper().writeValueAsString(employeeRq1))
 		)
-			.andExpect(MockMvcResultMatchers.content().string(jacksonObjectMapper().writeValueAsString(employee)))
+			.andExpect(MockMvcResultMatchers.content().string(jacksonObjectMapper().writeValueAsString(employeeRs1)))
 			.andReturn()
 
 		coVerify {
 			employeeService.save(any())
 		}
-	}
-
-	private companion object {
-		val employeeId = UUID.randomUUID().toString()
-		val organizationId = UUID.randomUUID().toString()
-		val createEmployeeRq =
-			EmployeeRq(
-				organizationId,
-				"Test",
-				"Test",
-				"Test"
-			)
-		val employee = EmployeeRs(
-			id = employeeId,
-			organizationId = organizationId,
-			firstName = "Test",
-			lastName = "Test",
-			jobTitle = "Test"
-		)
 	}
 }
